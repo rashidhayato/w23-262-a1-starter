fb772abbde73929ac27cce560238385fe0526352
Tony Grimes <acidtone@tonygrimes.com>
2023-02-07 07:04:23 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+
+/***********/
+/* Strings */
+/***********/
+
+// Define and assign stringBefore1 here
+
+// Define and assign stringBefore2 here
+
+// Using stringBefore1 and stringBefore2,
+// Define and assign stringAfter, using template literals, here
+
+
+/***********/
+/* Numbers */
+/***********/
+
+// Define and assign numberBefore1 here
+
+// Define and assign numberBefore2 here
+
+// Using numberBefore1 and numberBefore2,
+// Define and assign numberAfter, using template literals, here
+
+
+/************************************/
+/* Do not edit code below this line */
+/************************************/
+
+/* This code will create errors until the above steps are completed */
+
+// Print String Before 1
+const stringBefore1Output = document.querySelector('.string-before-1');
+stringBefore1Output.innerText = stringBefore1;
+
+// Print String Before 2
+const stringBefore2Output = document.querySelector('.string-before-2');
+stringBefore2Output.innerText = stringBefore2;
+
+// Print String After 2
+const stringAfterOutput = document.querySelector('.string-after');
+stringAfterOutput.innerText = stringAfter;
+
+// Print Number Before 1
+const numberBefore1Output = document.querySelector('.number-before-1');
+numberBefore1Output.innerText = numberBefore1;
+
+// Print Number Before 2
+const numberBefore2Output = document.querySelector('.number-before-2');
+numberBefore2Output.innerText = numberBefore2;
+
+// Print Number After
+const numberAfterOutput = document.querySelector('.number-after');
+numberAfterOutput.innerText = numberAfter;
